12acb569ad0bfd189700ad83d137af3f7e651224
TheNotary <no@email.plz>
2018-02-04 15:24:01 -0600

    tests a regex to read lines

--- lib/obs_recording_indicator.rb
+++ lib/obs_recording_indicator.rb
@@ -5,9 +5,6 @@ require 'file-tail'
 
 module ObsRecordingIndicator
   def self.monitor
-
-    puts "trying"
-
     # binding.pry
 
     log_path = 'C:\\Users\\john\\AppData\\Roaming\\obs-studio\\logs\\'
@@ -18,14 +15,30 @@ module ObsRecordingIndicator
 
     File.open(file_path) do |log|
       log.extend(File::Tail)
+      log.max_interval = 1
       log.interval # 10
-      log.backward(10)
+      log.backward(0)
 
       log.tail do |line|
-        puts line
+        time_stamp_sig = "^\d{2}:\d{2}:\d{2}\.\d{3}: " # a signature
+        start_sig = "==== Recording Start =+$"
+        stop_sig = "==== Recording Start =+$"
+
+        if line =~ /#{time_stamp_sig}#{start_sig}/
+          signal_start
+        elsif line =~ /#{time_stamp_sig}#{stop_sig}/
+          signal_stop
+        end
       end
 
     end
+  end
 
+  def signal_start
+    puts "IT STARTED"
+  end
+
+  def signal_stop
+    puts "IT STOPPED"
   end
 end
